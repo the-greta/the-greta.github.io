ee6de0b99bc56c417ccdedfa710f4ee0a8593d8c
dongwon kang <dw3624@gmail.com>
2024-01-02 15:44:04 +0900

    feat: add carousel

--- src/app/_components/carousel-card/carousel-card.tsx
+++ src/app/_components/carousel-card/carousel-card.tsx
@@ -1,30 +1,34 @@
+import Image from "next/image";
+
 import { Card, CardContent, CardHeader } from "@/components/ui/card/card";
 
 import styles from "./carousel-card.module.css";
 
 const CarouselCard = ({
+  imgUrl,
   index,
   title,
   desc,
 }: {
+  imgUrl: string;
   index: number;
   title: string;
   desc: string;
 }) => {
   return (
-    <>
-      <div className={styles.background}>bg</div>
+    <div className={styles.background}>
+      <Image src={imgUrl} alt="competence" width={1200} height={500} />
       <Card className={styles.container}>
         <CardHeader className={styles.header}>
           <div className={styles.title}>{title}</div>
           <div>
-            <span className={styles.index}>{index}</span>
+            <span className={styles.index}>0{index + 1}</span>
             /04
           </div>
         </CardHeader>
         <CardContent className={styles.content}>{desc}</CardContent>
       </Card>
-    </>
+    </div>
   );
 };
 
---
+++ src/app/_components/competence-carousel/competence-carousel.tsx
@@ -0,0 +1,97 @@
+import Autoplay from "embla-carousel-autoplay";
+import useEmblaCarousel from "embla-carousel-react";
+import React from "react";
+
+import CarouselCard from "@/app/_components/carousel-card/carousel-card";
+import { CarouselApi } from "@/components/ui/carousel/carousel";
+
+import styles from "./competence-carousel.module.css";
+
+const CAROUSEL_CARD_LIST = [
+  {
+    title: "통계학 전문 인력",
+    desc: "당사의 DS팀은 통계학 석박 과정을 이수 중이거나 완료한 전문인력으로 구성돼 있습니다.",
+    imgUrl: "/test.jpg",
+  },
+  {
+    title: "통계학 전문 인력",
+    desc: "당사의 DS팀은 통계학 석박 과정을 이수 중이거나 완료한 전문인력으로 구성돼 있습니다.",
+    imgUrl: "/test.jpg",
+  },
+  {
+    title: "통계학 전문 인력",
+    desc: "당사의 DS팀은 통계학 석박 과정을 이수 중이거나 완료한 전문인력으로 구성돼 있습니다.",
+    imgUrl: "/test.jpg",
+  },
+  {
+    title: "통계학 전문 인력",
+    desc: "당사의 DS팀은 통계학 석박 과정을 이수 중이거나 완료한 전문인력으로 구성돼 있습니다.",
+    imgUrl: "/test.jpg",
+  },
+];
+
+const CompetenceCarousel = () => {
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay()]);
+
+  const [canScrollPrev, setCanScrollPrev] = React.useState(false);
+  const [canScrollNext, setCanScrollNext] = React.useState(false);
+
+  const onSelect = React.useCallback((emblaApi: CarouselApi) => {
+    if (!emblaApi) return;
+    setCanScrollPrev(emblaApi.canScrollPrev());
+    setCanScrollNext(emblaApi.canScrollNext());
+  }, []);
+
+  const scrollPrev = React.useCallback(() => {
+    emblaApi?.scrollPrev();
+  }, [emblaApi]);
+
+  const scrollNext = React.useCallback(() => {
+    emblaApi?.scrollNext();
+  }, [emblaApi]);
+
+  const handleKeyDown = React.useCallback(
+    (event: React.KeyboardEvent<HTMLDivElement>) => {
+      if (event.key === "ArrowLeft") {
+        event.preventDefault();
+        scrollPrev();
+      } else if (event.key === "ArrowRight") {
+        event.preventDefault();
+        scrollNext();
+      }
+    },
+    [scrollPrev, scrollNext]
+  );
+
+  React.useEffect(() => {
+    if (!emblaApi) return;
+
+    onSelect(emblaApi);
+    emblaApi.on("reInit", onSelect);
+    emblaApi.on("select", onSelect);
+  }, [emblaApi, onSelect]);
+
+  return (
+    <div>
+      <div className="embla" ref={emblaRef} style={{ overflow: "hidden" }}>
+        <div className={styles["card-list-container"]}>
+          {CAROUSEL_CARD_LIST.map((card, i) => (
+            <div key={i} className={styles["card-container"]}>
+              <CarouselCard
+                index={i}
+                title={card.title}
+                desc={card.desc}
+                imgUrl={card.imgUrl}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* <div className="embla__buttons">
+        <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+        <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+      </div> */}
+    </div>
+  );
+};
+export default CompetenceCarousel;
