44dd31d32b01daa36d35f04544d3d55bbf41f584
dongwon kang <dw3624@gmail.com>
2024-01-03 16:17:03 +0900

    feat: add solution section

--- src/app/_components/solution-section/solution-section.tsx
+++ src/app/_components/solution-section/solution-section.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 import {
@@ -11,21 +12,21 @@ import styles from "./solution-section.module.css";
 const SOLUTION_CARD_LIST = [
   {
     title: "SYNTHEGRA",
-    desc: "All-in-One 합성데이터 생성 및 평가 솔루션",
+    desc: "All-in-One 합성데이터\n생성 및 평가 솔루션",
     tagList: ["합성", "증강"],
-    imgUrl: "",
+    imgUrl: "/solution/solution1.jpg",
   },
   {
     title: "MVC",
     desc: "기업 생태계 및 공급망 정보 시각화",
     tagList: ["수집", "시각화"],
-    imgUrl: "",
+    imgUrl: "/solution/solution2.jpg",
   },
   {
     title: "Data Analysis Consulting",
     desc: "데이터 분석·활용 컨설팅",
     tagList: ["분석", "예측"],
-    imgUrl: "",
+    imgUrl: "/solution/solution3.jpg",
   },
 ];
 
@@ -33,23 +34,33 @@ const SolutionCard = ({
   title,
   desc,
   tagList,
+  imgUrl,
 }: {
   title: string;
   desc: string;
   tagList: string[];
+  imgUrl: string;
 }) => {
   return (
-    <div className={styles.card}>
-      <div className={styles["card-header"]}>
-        {tagList.map((tag, i) => (
-          <span key={i} className={styles.tag}>
-            # {tag}
-          </span>
-        ))}
-      </div>
-      <div>
-        <h3 className={styles["solution-card-title"]}>{title}</h3>
-        <p className={styles["solution-card-sub"]}>{desc}</p>
+    <div className={styles.container}>
+      <Image
+        src={imgUrl}
+        alt="solution"
+        fill
+        style={{ objectFit: "cover", objectPosition: "center" }}
+      />
+      <div className={styles.card}>
+        <div className={styles["card-header"]}>
+          {tagList.map((tag, i) => (
+            <span key={i} className={styles.tag}>
+              # {tag}
+            </span>
+          ))}
+        </div>
+        <div>
+          <h3 className={styles["solution-card-title"]}>{title}</h3>
+          <p className={styles["solution-card-sub"]}>{desc}</p>
+        </div>
       </div>
     </div>
   );
@@ -57,20 +68,31 @@ const SolutionCard = ({
 
 const SolutionSection = () => {
   return (
-    <section className={styles.section}>
-      <SectionHeader>
-        <SectionTitle>solution</SectionTitle>
-        <SectionDesc>그레타의 데이터 솔루션</SectionDesc>
-      </SectionHeader>
-      <div className={styles["solution-container"]}>
-        {SOLUTION_CARD_LIST.map((card, i) => (
-          <SolutionCard
-            key={i}
-            title={card.title}
-            desc={card.desc}
-            tagList={card.tagList}
-          />
-        ))}
+    <section className={styles.section} id="solution">
+      <Image
+        src="/solution/solution-bg.jpg"
+        alt="solution-bg"
+        fill
+        quality={100}
+        priority={true}
+        style={{ objectFit: "cover", objectPosition: "center" }}
+      />
+      <div className={styles["section-container"]}>
+        <SectionHeader>
+          <SectionTitle theme="dark">solution</SectionTitle>
+          <SectionDesc theme="dark">그레타의 데이터 솔루션</SectionDesc>
+        </SectionHeader>
+        <div className={styles["solution-container"]}>
+          {SOLUTION_CARD_LIST.map((card, i) => (
+            <SolutionCard
+              key={i}
+              title={card.title}
+              desc={card.desc}
+              tagList={card.tagList}
+              imgUrl={card.imgUrl}
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
