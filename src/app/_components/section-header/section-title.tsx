ae947c0b3520e462c8191b5a16ebf8b9ca910563
dongwon kang <dw3624@gmail.com>
2024-01-03 16:17:18 +0900

    feat: add section header

--- src/app/_components/section-header/section-title.tsx
+++ src/app/_components/section-header/section-title.tsx
@@ -2,6 +2,13 @@ import React from "react";
 
 import styles from "./section-header.module.css";
 
+interface SectionDescType extends React.HTMLAttributes<HTMLParagraphElement> {
+  theme?: string;
+}
+interface SectionTitleType extends React.HTMLAttributes<HTMLHeadingElement> {
+  theme?: string;
+}
+
 const SectionHeader = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
@@ -10,16 +17,26 @@ const SectionHeader = React.forwardRef<
 ));
 SectionHeader.displayName = "SectionHeader";
 
-const SectionTitle = React.forwardRef<
-  HTMLHeadingElement,
-  React.HTMLAttributes<HTMLHeadingElement>
->(({ ...props }, ref) => <h2 ref={ref} {...props} className={styles.title} />);
+const SectionTitle = React.forwardRef<HTMLHeadingElement, SectionTitleType>(
+  ({ theme, ...props }, ref) => {
+    const styleClassName = [
+      styles.title,
+      theme == "dark" && styles["title-dark"],
+    ].join(" ");
+    return <h2 ref={ref} {...props} className={styleClassName} />;
+  }
+);
 SectionTitle.displayName = "SectionTitle";
 
-const SectionDesc = React.forwardRef<
-  HTMLParagraphElement,
-  React.HTMLAttributes<HTMLParagraphElement>
->(({ ...props }, ref) => <p ref={ref} {...props} className={styles.desc} />);
+const SectionDesc = React.forwardRef<HTMLParagraphElement, SectionDescType>(
+  ({ theme, ...props }, ref) => {
+    const styleClassName = [
+      styles.desc,
+      theme == "dark" && styles["desc-dark"],
+    ].join(" ");
+    return <p ref={ref} {...props} className={styleClassName} />;
+  }
+);
 SectionDesc.displayName = "SectionDesc";
 
 export { SectionDesc, SectionHeader, SectionTitle };
